cc6c33610f0b955c270904d583f816d127ec9bd8
Ivan <vanich7@mail.ru>
2022-10-07 12:24:14 +0300

    Исправление

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Калькулятор на React, типизация TypeScript, бизнеc логика реализованна на Redux, стили написаны на scss
 
+#### [Посмотреть](https://vanichh.github.io/test-oxem/)
+
 ![Alt Text](https://s4.gifyu.com/images/bandicam-2022-10-04-12-23-11-551.gif)
 
 #### Установка
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import { App } from './app';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from 'store/store';
 import { Provider } from 'react-redux';
